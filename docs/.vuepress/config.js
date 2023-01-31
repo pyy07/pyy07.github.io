95c818c28cc379b81b389ad798e1c02c0cccb6cb
panyiyong <panyiyong@gf.com.cn>
2023-01-31 16:01:19 +0800

    github page

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    title: 'LukePan',
+    description: 'LukePan的文档',
+    base: '/pyy07.github.io/',
+    locales: {
+        '/': {
+            lang: 'zh-CN'
+        }
+    },
+    themeConfig: {
+        subSidebar: 'auto',
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '金融', link: '/' },
+            { text: '电商', link: '/' },
+            { text: '游戏', link: '/' },
+            { text: '通信', link: '/' },
+        ],
+        sidebar: [
+            {
+                title: '欢迎',
+                path: '/',
+            },
+        ]
+    }
+}
